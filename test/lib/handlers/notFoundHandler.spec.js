20df30863f9c09720fc8398c944093cc3ab98c41
Cristoph Adam Sugianto <sugianto@btpn.com>
2020-02-28 17:12:04 +0700

    create unit test notFoundHandler

---
+++ test/lib/handlers/notFoundHandler.spec.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const { expect } = chai;
+const sinon = require('sinon');
+chai.should();
+
+const { notFoundHandler } = require('../../../lib/handlers');
+const { NotFoundError } = require('../../../lib/errors')
+
+describe('notFoundHandler', () => {
+  let res;
+  let req;
+  let next;
+
+  beforeEach(() => {
+    req = {
+      route: true
+    };
+    res = {
+      status: sinon.stub().returns({
+        send: sinon.stub()
+      })
+    };
+    next = sinon.stub();
+  });
+
+  it('should passed if route found', () => {
+    notFoundHandler(req, res, next);
+    expect(next.calledOnce).to.equal(true);
+  });
+
+  it('should throw NOT_FOUND error if route not found', () => {
+    req.route = false;
+    expect(() => notFoundHandler(req, res, next)).to.throw(
+      Error,
+      'Page you\'re request not found'
+    );;
+  });
+});
